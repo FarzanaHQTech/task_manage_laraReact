9556549a6c7b1054c5006bb3916ddbe82a6bb4bc
FarzanaHQTech <farzana.tech24@gmail.com>
2025-05-01 11:25:20 +0600

    setup

---
+++ app/Models/TaskList.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class TaskList extends Model
+{
+    protected $fillable = ['title', 'description', 'user_id'];
+    public function task(): HasMany
+    {
+        return $this->hasMany(Task::class, 'list_id');
+    }
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
